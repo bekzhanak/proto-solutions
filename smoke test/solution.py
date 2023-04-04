0fdcc69fc9f31456000acc1e9a6d6bce66555bb2
Bekzhan Aktoreyev <bekzhanaktureev@gmail.com>
2023-04-04 18:05:27 +0600

    smoke test

---
+++ smoke test/solution.py
@@ -0,0 +1,15 @@
+import socket
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind(("0.0.0.0", 8000))
+socket.listen(10)
+
+while True:
+    conn, _ = socket.accept()
+    with conn:
+        buffer = b""
+        data = conn.recv(1024)
+        if not data:
+            conn.sendall(buffer)
+        buffer += data
+        
